99276560268ebf49dfb20cd42675f2141c5fb0d3
mariom6 <91935255+mariom6@users.noreply.github.com>
2023-11-18 14:18:54 -0500

    First upload of local files

--- gradeAnalysisWidgets.py
+++ gradeAnalysisWidgets.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from functools import partial
 import sys
+<<<<<<< HEAD
 import logging
 import platform
 import numpy as np
@@ -138,10 +139,15 @@ class Logger(logging.Logger):
 
     def critical(self, message):
         self.log(logging.CRITICAL, message)
+=======
+import platform
+import subprocess
+>>>>>>> fe8c6a2 (First upload of local files)
 
 
 class ConfirmButton:
     def __init__(self):
+<<<<<<< HEAD
         self.logger = Logger(__name__)  # Create a logger using the custom Logger class
         self.confirm_button = None
         self.logger.info("ConfirmButton instance initialized")
@@ -171,10 +177,25 @@ class ConfirmButton:
         widget.bind("<Enter>", lambda event: tooltip.showtip())
         widget.bind("<Leave>", lambda event: tooltip.hidetip())
         self.logger.debug("Tooltip events bound successfully")
+=======
+        self.confirm_button = None
+
+    def make_confirm_button(self, where, title="Confirm", command=None, row=int, column=int, helptip=""):
+        self.confirm_button = tk.Button(where, text=title, command=command)
+        self.confirm_button.grid(row=row, column=column)
+        self.bind_tooltip_events(self.confirm_button, helptip)
+    
+    def bind_tooltip_events(self, widget, text):
+        # Bind tooltip show and hide events to a widget
+        tooltip = ToolTip(widget, text)
+        widget.bind("<Enter>", lambda event: tooltip.showtip())
+        widget.bind("<Leave>", lambda event: tooltip.hidetip())
+>>>>>>> fe8c6a2 (First upload of local files)
 
 
 class TableWidget:
     def __init__(self):
+<<<<<<< HEAD
         self.logger = Logger(__name__)  # Create a logger for this class
         self.table = None
         self.logger.info("TableWidget instance initialized")
@@ -227,10 +248,48 @@ class TableWidget:
 
     def destroy(self):
         self.logger.debug("Destroying table widget")
+=======
+        self.table = None
+
+    def generic_tableview_widget(self, where, row=int, column=int, title="", colHeading="", helptip=""):
+        self.table = ttk.Treeview(where, columns = colHeading, show='headings', selectmode=tk.BROWSE) #Table view for all options
+        self.table.grid(row=row, column=column) 
+        self.table.heading(colHeading, text=title)
+        self.bind_tooltip_events(self.table, helptip)
+        return self.table
+    
+    def insert(self, parent, index, values=()):
+        if self.table is not None:
+            self.table.insert(parent=parent, index=index, values=values)
+
+    def selection(self):
+        if self.table is not None:
+            selected = self.table.selection()
+            if selected:
+                return selected
+            else:
+                print(f"No selection found.")
+        else:
+            print("Table is not initialized.")
+
+    def item(self, selected_item):
+        if self.table is not None:
+            item_info = self.table.item(selected_item)
+            values = item_info.get('values', ())
+            if values:
+                return values[0]  # Return the first value, if it exists
+            else:
+                print(f"No values found for item: {selected_item}")
+        else:
+            print("Table is not initialized.")
+
+    def destroy(self):
+>>>>>>> fe8c6a2 (First upload of local files)
         if self.table is not None:
             self.table.destroy()
 
     def grid_forget(self):
+<<<<<<< HEAD
         self.logger.debug("Forgetting grid placement of table")
         if self.table is not None:
             self.table.grid_forget()
@@ -258,10 +317,28 @@ class ThresholdWidget:
         self.logger.info(
             f"Creating threshold widget with label '{text}' at row {row}, column {column}"
         )
+=======
+        self.table.grid_forget()
+
+    
+    def bind_tooltip_events(self, widget, text):
+        # Bind tooltip show and hide events to a widget
+        tooltip = ToolTip(widget, text)
+        widget.bind("<Enter>", lambda event: tooltip.showtip())
+        widget.bind("<Leave>", lambda event: tooltip.hidetip())
+
+class ThresholdWidget:
+    def __init__(self):
+        self.label = None
+        self.entry = None
+    
+    def generic_thresholds_widget(self, where, state=str, text=str, row=int, column=int, help=str):
+>>>>>>> fe8c6a2 (First upload of local files)
         self.label = tk.Label(where, text=text)
         self.label.grid(row=row, column=column, sticky=tk.W)
         self.entry = tk.Entry(where, width=3)
         self.entry.config(state=state)
+<<<<<<< HEAD
         self.entry.grid(row=row, column=column + 1, sticky=tk.W)
         self.bind_tooltip_events(self.entry, help)
         self.logger.debug("Threshold widget created and configured")
@@ -280,10 +357,23 @@ class ThresholdWidget:
 
     def destroy(self):
         self.logger.debug("Destroying threshold widget components")
+=======
+        self.entry.grid(row=row, column=column+1, sticky=tk.W)
+        self.bind_tooltip_events(self.entry, help)
+
+    def get_entry_value(self):
+        if self.entry.get()!="":
+            return int(self.entry.get())
+        else:
+            return
+
+    def destroy(self):
+>>>>>>> fe8c6a2 (First upload of local files)
         if self.label is not None:
             self.label.destroy()
         if self.entry is not None:
             self.entry.destroy()
+<<<<<<< HEAD
         self.logger.info("Threshold widget components destroyed")
 
     def bind_tooltip_events(self, widget, text):
@@ -348,28 +438,84 @@ class CheckboxWidget:
 class Console(tk.Text):
     def __init__(self, *args, **kwargs):
         self.logger = Logger(__name__)  # Create a logger for this class
+=======
+
+
+    def bind_tooltip_events(self, widget, text):
+        # Bind tooltip show and hide events to a widget
+        tooltip = ToolTip(widget, text)
+        widget.bind("<Enter>", lambda event: tooltip.showtip())
+        widget.bind("<Leave>", lambda event: tooltip.hidetip())
+
+class CheckboxWidget:
+    def __init__(self):
+        self.checkboxes = {}
+
+    def create_checkbox(self, text, help_text, state, where, row, column):
+        checkbox_state = tk.BooleanVar()
+        checkbox = tk.Checkbutton(where, state=state, text=text, variable=checkbox_state)
+        checkbox.grid(row=row, column=column, sticky=tk.W)
+        self.bind_tooltip_events(checkbox, help_text)
+        self.checkboxes[text] = checkbox_state
+
+    def create_multiple_checkboxes(self, options, state, where, row, column):
+        for idx, (text, help_text) in enumerate(options.items(), start=row):
+            self.create_checkbox(text, help_text, state, where, idx, column)
+
+    def get_selected_analyses(self):
+        selected = {text: state.get() for text, state in self.checkboxes.items()}
+        return selected
+
+    def destroy(self):
+            self.checkboxes.clear()
+
+    def bind_tooltip_events(self, widget, text):
+        tooltip = ToolTip(widget, text)
+        widget.bind("<Enter>", lambda event: tooltip.showtip())
+        widget.bind("<Leave>", lambda event: tooltip.hidetip())
+    
+
+class Console(tk.Text):
+    def __init__(self, *args, **kwargs):
+>>>>>>> fe8c6a2 (First upload of local files)
         kwargs.update({"state": "disabled"})
         tk.Text.__init__(self, *args, **kwargs)
         self.bind("<Destroy>", self.reset)
         self.old_stdout = sys.stdout
         sys.stdout = self
+<<<<<<< HEAD
         self.logger.info("Console widget initialized and stdout redirected")
 
     def delete(self, *args, **kwargs):
         self.logger.debug("Clearing console text")
         self.config(state="normal")
         super().delete(*args, **kwargs)  # Use super() to avoid recursion
+=======
+
+    def delete(self, *args, **kwargs):
+        self.config(state="normal")
+        self.delete(*args, **kwargs)
+>>>>>>> fe8c6a2 (First upload of local files)
         self.config(state="disabled")
 
     def write(self, content):
         self.config(state="normal")
         self.insert(tk.END, content)
+<<<<<<< HEAD
         self.see(tk.END)
+=======
+        self.see("end")
+>>>>>>> fe8c6a2 (First upload of local files)
         self.config(state="disabled")
 
     def reset(self, event):
         sys.stdout = self.old_stdout
+<<<<<<< HEAD
         self.logger.info("Console widget destroyed and stdout reset")
+=======
+
+
+>>>>>>> fe8c6a2 (First upload of local files)
 
 
 class ToolTip:
@@ -399,16 +545,23 @@ class ToolTip:
 
 class FileOpener:
     def __init__(self, file_path):
+<<<<<<< HEAD
         self.logger = Logger(__name__)
         self.file_path = file_path
         self.logger.info(f"FileOpener instance created for file: {file_path}")
 
     def print(self):
         self.logger.debug(f"Printing file path: {self.file_path}")
+=======
+        self.file_path = file_path
+
+    def print(self):
+>>>>>>> fe8c6a2 (First upload of local files)
         print(self.file_path)
 
     def open_file(self, *args):
         current_platform = platform.system()
+<<<<<<< HEAD
         self.logger.info(f"Attempting to open file on {current_platform} platform")
         if current_platform == "Linux":
             subprocess.Popen(["xdg-open", self.file_path])
@@ -1346,3 +1499,15 @@ class tkScrolledtextBox(tkst.ScrolledText):
             file.write(self.get_text())
 
     
+=======
+        if current_platform == "Linux":
+            subprocess.Popen(["xdg-open", self.file_path])
+        elif current_platform == "Windows":
+            subprocess.Popen(["cmd", "/c", "start", self.file_path], shell=True)
+        elif current_platform == "Darwin":  # macOS
+            subprocess.Popen(["open", self.file_path])
+        else:
+            return
+        
+    
+>>>>>>> fe8c6a2 (First upload of local files)
